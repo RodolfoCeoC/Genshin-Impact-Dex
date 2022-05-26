1362d4399570e93f29a839706fd77a86fe031713
Rodolfo ceo cortes <ceosmarck@gmail.com>
2022-05-26 18:33:48 -0500

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
+import {useState} from "react";
 
+const tipos = {
+artifacts: "Artefactos",
+boss: "Jefes",
+characters: "Personajes",
+consumables: "Consumibles",
+domains: "Dominios",
+elements: "Elementos",
+enemies: "Enemigos",
+materials: "Materiales",
+nations: "Naciones",
+weapons: "Armas"
+};
 
 function App() {
-  const opciones = async () => {
-   const respuesta = await fetch("https://api.genshin.dev/");
-   const types = await respuesta.json();
-    console.log({types});
-  };
 
-  opciones();
+  const [genshinState, setGenshinState] = useState({
+    types: [],
+  });
+
+  const fetchGenshinApi = async (item, url = "https://api.genshin.dev/") => {
+   const respuesta = await fetch(url);
+   const respJson = await respuesta.json();
+   if (item === "types") {
+     setGenshinState({
+      types: respJson.types,
+     });
+     
+   } else{
+     setGenshinState({
+       [item]: respJson,
+     });
+   }   
+  };
+  
+  fetchGenshinApi("types");
+
+  const handleChangeType = ({target}) => {
+    const url = `https://api.genshin.dev/${target.value}`;
+    fetchGenshinApi(target.value, url);
+    console.log(genshinState);
+  };
 
   return (
     <div className="App container">
       <h1>Genshin Impact</h1>
       <hr />
-      <select>
+      <select name="types" onChange={handleChangeType}>
         <option value="">Seleccione un elemento
         </option>
-        <option value="artifiacts">artifacts</option>
-        <option value="boss">boss</option>
-        <option value="characters">character</option>
-        <option value="consumables">consumables</option>
-        <option value="domains">domains</option>
-        <option value="elements">elements</option>
-        <option value="enemies">enemies</option>
-        <option value="materials">materials</option>
-        <option value="nations">nations</option>
-        <option value="weapons">weapons</option>
+        {genshinState.types.map((type) => (
+          <option key={type} value={type}>
+            {tipos[type]}
+            </option>
+        ))}          
       </select>
     </div>
   );
